38b5e04818a268c198fe5637d3e745c84aee99df
LukasAnell <lukasmanell@gmail.com>
2023-12-11 10:04:45 -0800

    Still trying Day 5 Pt 2.

--- src/dayFive/AoC5.java
+++ src/dayFive/AoC5.java
@@ -10,7 +10,7 @@ public class AoC5 {
     public static List<BigInteger> seeds = new ArrayList<>();
 
     public static void main(String[] args) {
-        List<String> lines = Reader.readFromFile("src/dayFive/Day5Input.txt");
+        List<String> lines = Reader.readFromFile("src/dayFive/Day5TestInput.txt");
         assert lines != null;
 
         makeMappings(lines);
@@ -24,15 +24,69 @@ public class AoC5 {
     }
 
     public static void partTwo() {
+        Map<BigInteger, BigInteger[]> seedRangeMap = new HashMap<>();
+        List<BigInteger[]> seedRanges = new ArrayList<>();
+        for(int i = 0; i < seeds.size() - 1; i += 2) {
+            BigInteger currentSeed = seeds.get(i);
+            BigInteger[] range = new BigInteger[]{currentSeed, currentSeed.add(seeds.get(i + 1))};
+            seedRanges.add(range);
+            seedRangeMap.put(currentSeed, range);
+            seedRangeMap.put(currentSeed.add(seeds.get(i + 1)), range);
+        }
 
+        BigInteger minSeed = BigInteger.valueOf(Integer.MAX_VALUE);
+        for(BigInteger[] range: seedRanges) {
+            minSeed = minSeed.min(checkRangeBounds(range));
+        }
+        System.out.println(minSeed);
+        System.out.println(seedRangeMap);
+
+        BigInteger minLocation = new BigInteger(String.valueOf(Integer.MAX_VALUE));
+        BigInteger[] range = seedRangeMap.get(minSeed);
+        System.out.println(Arrays.toString(range));
+        for(BigInteger i = range[0]; i.compareTo(range[1]) <= 0; i = i.add(BigInteger.ONE)) {
+            BigInteger seed = i;
+            for(List<BigInteger[]> map: mappings) {
+                for(BigInteger[] mapping: map) {
+                    BigInteger destinationStart = mapping[0];
+                    BigInteger sourceStart = mapping[1];
+                    BigInteger length = mapping[2];
+                    if (seed.compareTo(sourceStart) >= 0 && seed.compareTo(sourceStart.add(length)) < 0) {
+                        seed = destinationStart.add(seed.subtract(sourceStart));
+                        break;
+                    }
+                }
+            }
+            minLocation = minLocation.min(seed);
+        }
+        System.out.println(minLocation);
+    }
+
+    public static BigInteger checkRangeBounds(BigInteger[] range) {
+        BigInteger minSeed = range[0];
+        for(BigInteger seed : range) {
+            for(List<BigInteger[]> map: mappings) {
+                for(BigInteger[] mapping: map) {
+                    BigInteger destinationStart = mapping[0];
+                    BigInteger sourceStart = mapping[1];
+                    BigInteger length = mapping[2];
+                    if(seed.compareTo(sourceStart) >= 0 && seed.compareTo(sourceStart.add(length)) < 0) {
+                        seed = destinationStart.add(seed.subtract(sourceStart));
+                        break;
+                    }
+                }
+            }
+            minSeed = minSeed.min(seed);
+        }
+        return minSeed;
     }
 
     public static void partOne() {
         BigInteger minLocation = new BigInteger(String.valueOf(Integer.MAX_VALUE));
-        for (BigInteger seed: seeds) {
+        for(BigInteger seed: seeds) {
             BigInteger location = seed;
-            for (List<BigInteger[]> map: mappings) {
-                for (BigInteger[] mapping: map) {
+            for(List<BigInteger[]> map: mappings) {
+                for(BigInteger[] mapping: map) {
                     BigInteger destinationStart = mapping[0];
                     BigInteger sourceStart = mapping[1];
                     BigInteger length = mapping[2];
--- src/daySeven/AoC7.java
+++ src/daySeven/AoC7.java
@@ -2,11 +2,13 @@ package daySeven;
 
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import Utils.Reader;
 
 public class AoC7 {
-    public static Map<String, Integer> partOneHandMap = new HashMap<>() {{
+    public static Map<String, Integer> handMap = new HashMap<>() {{
         put("A", 14);
         put("K", 13);
         put("Q", 12);
@@ -21,104 +23,13 @@ public class AoC7 {
         put("3", 3);
         put("2", 2);
     }};
-
-    public static Map<String, Integer> partTwoHandMap = new HashMap<>() {{
-        put("A", 14);
-        put("K", 13);
-        put("Q", 12);
-        put("T", 10);
-        put("9", 9);
-        put("8", 8);
-        put("7", 7);
-        put("6", 6);
-        put("5", 5);
-        put("4", 4);
-        put("3", 3);
-        put("2", 2);
-        put("J", 1);
-    }};
-
     public static void main(String[] args) {
-        List<String> lines = Reader.readFromFile("src/daySeven/Day7TestInput.txt");
+        List<String> lines = Reader.readFromFile("src/daySeven/Day7Input.txt");
         assert lines != null;
 
         partOne(lines);
-        // 252656917 answer
-        partTwo(lines);
-    }
-
-    public static void partTwo(List<String> lines) {
-        List<String> handList = new ArrayList<>();
-        Map<String, Integer> handToBidMap = new HashMap<>();
-
-        for(String line: lines) {
-            String hand = line.split(" ")[0];
-            int bid = Integer.parseInt(line.split(" ")[1]);
-            handToBidMap.put(hand, bid);
-            handList.add(hand);
-        }
-
-        handList.sort(Comparator.comparingInt(AoC7::findHandType2));
-        for(int i = 0; i < handList.size() / 2; i++) {
-            sortHandsByCards2(handList);
-        }
-
-        int sum = 0;
-        for(int i = 0; i < handList.size(); i++) {
-            String hand = handList.get(i);
-            sum += handToBidMap.get(hand) * (i + 1);
-        }
-        System.out.println(sum);
-    }
-
-    public static void sortHandsByCards2(List<String> handList) {
-        for(int i = 0; i < handList.size() - 1; i++) {
-            String hand1 = handList.get(i);
-            String hand2 = handList.get(i + 1);
-            if(findHandType(hand1) == findHandType(hand2)) {
-                for(int j = 0; j < hand1.length(); j++) {
-                    if(hand1.charAt(j) != hand2.charAt(j)) {
-                        int hand1Card = partTwoHandMap.get(Character.toString(hand1.charAt(j)));
-                        int hand2Card = partTwoHandMap.get(Character.toString(hand2.charAt(j)));
-
-                        if(hand1Card > hand2Card) {
-                            handList.set(i + 1, hand1);
-                            handList.set(i, hand2);
-                        }
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    public static int findHandType2(String hand) {
-        Map<Character, Integer> frequency = hand.chars()
-            .mapToObj(i -> (char) i)
-            .collect(HashMap::new, (m, k) -> m.merge(k, 1, Integer::sum), Map::putAll);
-
-        if(frequency.size() == 1 && frequency.containsValue(5)) {
-            return 7;
-        }
-        if(frequency.size() == 2 && frequency.containsValue(4)) {
-            return 6;
-        }
-        if(frequency.containsValue(3) && frequency.containsValue(2)) {
-            return 5;
-        }
-        if(frequency.size() == 3 && frequency.containsValue(3)) {
-            return 4;
-        }
-        if(frequency.size() == 3 && Collections.frequency(frequency.values(), 2) == 2) {
-            return 3;
-        }
-        if(frequency.size() == 4 && frequency.containsValue(2)) {
-            return 2;
-        }
-        if(frequency.size() == 5) {
-            return 1;
-        }
-        return Collections.max(frequency.values());
+        // 251290417 too low
+        // 253341391 too high
     }
 
     public static void partOne(List<String> lines) {
@@ -132,12 +43,15 @@ public class AoC7 {
             handList.add(hand);
         }
 
-        handList.sort(Comparator.comparingInt(AoC7::findHandType));
+        for(int i = 0; i < handList.size(); i++) {
+            handList.sort(Comparator.comparingInt(AoC7::findHandType));
+        }
         for(int i = 0; i < handList.size() / 2; i++) {
             sortHandsByCards(handList);
         }
 
         int sum = 0;
+        System.out.println(handList);
         for(int i = 0; i < handList.size(); i++) {
             String hand = handList.get(i);
             sum += handToBidMap.get(hand) * (i + 1);
@@ -152,8 +66,8 @@ public class AoC7 {
             if(findHandType(hand1) == findHandType(hand2)) {
                 for(int j = 0; j < hand1.length(); j++) {
                     if(hand1.charAt(j) != hand2.charAt(j)) {
-                        int hand1Card = partOneHandMap.get(Character.toString(hand1.charAt(j)));
-                        int hand2Card = partOneHandMap.get(Character.toString(hand2.charAt(j)));
+                        int hand1Card = handMap.get(hand1.substring(j, j + 1));
+                        int hand2Card = handMap.get(hand2.substring(j, j + 1));
 
                         if(hand1Card > hand2Card) {
                             handList.set(i + 1, hand1);
@@ -167,31 +81,11 @@ public class AoC7 {
     }
 
     public static int findHandType(String hand) {
-        Map<Character, Integer> frequency = hand.chars()
-            .mapToObj(i -> (char) i)
-            .collect(HashMap::new, (m, k) -> m.merge(k, 1, Integer::sum), Map::putAll);
+        Map<Character, Integer> frequency =
+                hand.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(c -> 1)));
 
-        if(frequency.size() == 1 && frequency.containsValue(5)) {
-            return 7;
-        }
-        if(frequency.size() == 2 && frequency.containsValue(4)) {
-            return 6;
-        }
-        if(frequency.containsValue(3) && frequency.containsValue(2)) {
-            return 5;
-        }
-        if(frequency.size() == 3 && frequency.containsValue(3)) {
-            return 4;
-        }
-        if(frequency.size() == 3 && Collections.frequency(frequency.values(), 2) == 2) {
-            return 3;
-        }
-        if(frequency.size() == 4 && frequency.containsValue(2)) {
-            return 2;
-        }
-        if(frequency.size() == 5) {
-            return 1;
-        }
-        return Collections.max(frequency.values());
+        return Collections.max(frequency.entrySet(), Map.Entry.comparingByValue()).getValue();
     }
 }
